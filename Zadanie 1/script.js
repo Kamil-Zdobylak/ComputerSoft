dae5e96468198e4a033b6e12ce5e9bddc68ee4e1
Kamil Zdobylak <kamil.zdobylak98@gmail.com>
2022-07-19 13:58:15 +0200

    Task complete!

---
+++ Zadanie 1/script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    let inputValue = prompt('DOM Name');
+    if (inputValue.length == 0){
+        alert("nie moze byc pusty");
+    } else {
+        let elements = document.querySelectorAll(inputValue);
+
+        elements.forEach(function(element){
+           console.log(element.outerText);
+        });
+    }
+   };
